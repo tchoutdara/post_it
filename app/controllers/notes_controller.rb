d9d0466960be2129fbaf172286404450771a7566
tchoutdara <monkeymilitia444@gmail.com>
2018-08-30 15:21:00 -0600

    add new page and link, add show item from db

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,30 @@
+class NotesController < ApplicationController
+  def index
+    @notes = Note.all
+  end
+
+  def show
+    @note = Note.find(params[:id])
+  end
+
+  def new
+    @note = Note.new
+  end
+
+  def create
+    @note = Note.new(notes_params)
+
+    if @note.save
+      redirect_to notes_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+   def notes_params
+    params.require(:note).permit(:title, :author, :body)
+    end
+
+end
